3714166459708a67f817d3026276413f2f9238d7
PavelSteel <misskiisteel@gmail.com>
2023-02-21 23:36:40 +0300

    Dependency Injection with setter

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -2,17 +2,35 @@ package spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
     public Person() {
     }
 
-    public Person(Pet pet) {
-        this.pet = pet;
-    }
+    // public Person(Pet pet) {
+    //     this.pet = pet;
+    // }
 
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void callYourPet() {
