8ea9b31fadc89c5bf85a3cc2e2c0335d17ab146c
PavelSteel <misskiisteel@gmail.com>
2023-02-23 18:09:33 +0300

    Test4 / Studying the topic Bean scope, singleton

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,6 +1,10 @@
 package spring_introduction;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("Создана собака!");
+    }
+
     @Override
     public void say(){
         System.out.println("Wow-wow!");
